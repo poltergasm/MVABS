029b52c49fb19ff8aa2ed921b9e2ba182b4dc091
Poltergasm <memoize@gmx.com>
2018-12-29 18:24:23 +0000

    Can only switch to skills you know now

--- ABS.js
+++ ABS.js
@@ -1,6 +1,6 @@
 (function() {
 
-    Scene_Boot.prototype.start = function() {
+    /*Scene_Boot.prototype.start = function() {
         Scene_Base.prototype.start.call(this);
         SoundManager.preloadImportantSounds();
         if (DataManager.isBattleTest()) {
@@ -13,6 +13,33 @@
         }
         this.updateDocumentTitle();
         $gamePlayer._currentSpell = 8;
+    };*/
+
+    var ABS = ABS || {};
+    ABS.Player_Spells = {
+        all: ['heal', 'fireball'],
+        known: []
+    };
+
+    ABS.Projectiles = [];
+
+    Sprite.prototype.createProjectile = function(x, y) {
+        var _img = ImageManager.loadPicture("projectile");
+        var _sprite = new Sprite(this._img);
+        _sprite.x = x;
+        _sprite.y = y;
+        this.addChild(_sprite);
+        ABS.Projectiles.push(_sprite);
+    };
+
+    var GamePlayer_initMembers = Game_Player.prototype.initMembers;
+    Game_Player.prototype.initMembers = function() {
+        GamePlayer_initMembers.call(this);
+
+        this._currentSpell = 0;
+        this._skillId = $gameActors.actor(1)._skills[this._currentSpell];
+        this._immunity = 60;
+        ABS.Player_Spells.known = ['heal', 'fireball'];
     };
 
     var _GameEvent_initialize = Game_Event.prototype.initialize;
@@ -58,6 +85,7 @@
                     });
                     
                     if ($dataMap.events[this._eventId].pages.length > 1) {
+                        $dataMap.events[this._eventId].meta = {};
                         var _mapId = $gameMap._mapId;
                         $gameSelfSwitches.setValue([_mapId, this._eventId, 'A'], true);
 
@@ -66,8 +94,6 @@
                         this.erase();
                     }
                     this._eventEnemyAlive = false;
-                    //$dataMap.events[this._eventId] = $dataMap.events[1];
-                    //this.obj = $dataMap.events[1];
                 }
             }
         }
@@ -84,16 +110,16 @@
                     this._eventEnemyTouching += 1;
                     var _weaponId = $gameActors.actor(1)._equips[0]._itemId;
                     if (Input.isTriggered('ok') && _weaponId > 0) {  
-                        this.requestAnimation(6);
-                        AudioManager.playSe({
+                        this.requestAnimation(121);
+                        /*AudioManager.playSe({
                             name: 'MDSFX_FoeAtk_1_0',
                             pan: 0,
                             pitch: 100,
                             volume: 55
-                        });
+                        });*/
                         this.setMoveSpeed(6);
-                        this.moveAwayFromPlayer();
-                        this.moveAwayFromPlayer();
+                        this.moveBackward();
+                        this.moveBackward();
                         this.setMoveSpeed(4);
                         this.setTransparent(true);
                         this.setTransparent(false);
@@ -103,8 +129,7 @@
                         this._eventEnemyHp -= _dmg;
                         //$dataMap.events[this._eventId]._eventEnemyHp -= _dmg;
                     } else {
-                        // enemy reaction time
-                        if (this._eventEnemyTouching >= 20 && !this._prop) {
+                        if (this._eventEnemyTouching >= 20 && Number($gamePlayer._immunity) >= 60 && !this._prop) {
                             $gamePlayer.requestAnimation(1);
                             $gamePlayer.setTransparent(true);
                             $gamePlayer.setTransparent(false);
@@ -113,6 +138,7 @@
                             $gamePlayer.moveBackward();
                             $gamePlayer.moveBackward();
                             $gameActors.actor(1).gainHp(-50);
+                            $gamePlayer._immunity = 0;
                         }
                     }
                 } else {
@@ -127,59 +153,81 @@
         _SceneMap_start.call(this);
         this._healthBar = new HUD(100,100);
         this.addWindow(this._healthBar);
+        /*var bitmap = ImageManager.loadPicture('projectile');
+        var sprite = new Sprite(bitmap);
+        sprite.x = 100;
+        sprite.y = 100;
+        this.addChild(sprite);*/
     };
+
+    function _shootFireball(_skill) {
+        var _eid = -1;
+        for (var i = 0; i < 5; i++) {
+            switch(Number($gamePlayer._direction)) {
+                case 2:
+                    // down
+                    _eid = $gameMap.eventIdXy($gamePlayer.x, $gamePlayer.y+i);
+                    break;
+                case 6:
+                    // right
+                    _eid = $gameMap.eventIdXy($gamePlayer.x+i, $gamePlayer.y); 
+                    break;
+                case 8:
+                    // up
+                    _eid = $gameMap.eventIdXy($gamePlayer.x, $gamePlayer.y-i);
+                    break;
+                case 4:
+                    // left
+                    _eid = $gameMap.eventIdXy($gamePlayer.x-i, $gamePlayer.y); 
+                    break;
+            }
+            
+            if (_eid > 0) {
+               var _ev = $dataMap.events[_eid];
+               if (_ev.meta.enemy) {
+                    if (_ev.obj !== 'undefined') {
+                        _ev.obj.requestAnimation(_skill.animationId);
+                        _ev.obj._eventEnemyHp -= 100;
+                    }
+                }
+            }
+        }
+    }
 
     var _SceneMap_update = Scene_Map.prototype.update;
     Scene_Map.prototype.update = function() {
         _SceneMap_update.call(this);
 
+        if ($gamePlayer._immunity < 60) $gamePlayer._immunity += 1;
         if (Input.isTriggered('shift')) {
-            var _skill = $dataSkills[$gamePlayer._currentSpell];
-            if (_skill.meta) {
+            var _skill = $dataSkills[$gamePlayer._skillId];
+            if (_skill && _skill.meta) {
                 if (_skill.meta.heal) {
                     $gameActors.actor(1).gainHp(Number(_skill.meta.heal));
                     $gamePlayer.requestAnimation(_skill.animationId);
                 }
 
                 if (_skill.meta.fireball) {
-                    var _eid = -1;
-                    switch(Number($gamePlayer._direction)) {
-                        case 2:
-                            // down
-                            _eid = $gameMap.eventIdXy($gamePlayer.x, $gamePlayer.y+2);
-                            break;
-                        case 6:
-                            // right
-                            _eid = $gameMap.eventIdXy($gamePlayer.x+2, $gamePlayer.y); 
-                            break;
-                        case 8:
-                            // up
-                            _eid = $gameMap.eventIdXy($gamePlayer.x, $gamePlayer.y-2);
-                            break;
-                        case 4:
-                            // left
-                            _eid = $gameMap.eventIdXy($gamePlayer.x-2, $gamePlayer.y); 
-                            break;
-                    }
-                    
-                    if (_eid > 0) {
-                       var _ev = $dataMap.events[_eid];
-                       if (_ev.meta.enemy) {
-                            if (_ev.obj !== 'undefined') {
-                                _ev.obj.requestAnimation(_skill.animationId);
-                                _ev.obj._eventEnemyHp -= 100;
-                            }
-                       }
-                    }
+                    _shootFireball(_skill);
                 }
             }
         }
 
         // cycle through skills
         if (Input.isTriggered('pagedown')) {
-            $gamePlayer._currentSpell += 1;
+            var _len = $gameActors.actor(1)._skills.length-1;
+            if ($gamePlayer._currentSpell+1 > _len) {
+                $gamePlayer._currentSpell = 0;
+            } else {
+                $gamePlayer._currentSpell += 1;
+            }
+
+            $gamePlayer._skillId = $gameActors.actor(1)._skills[$gamePlayer._currentSpell];
         } else if (Input.isTriggered('pageup')) {
-            $gamePlayer._currentSpell -= 1;
+            if ($gamePlayer._currentSpell-1 >= 0) {
+                $gamePlayer._currentSpell -= 1;
+                $gamePlayer._skillId = $gameActors.actor(1)._skills[$gamePlayer._currentSpell];
+            }
         }
 
         this._healthBar.refresh();
@@ -204,11 +252,15 @@
 
     HUD.prototype.refresh = function() {
         this.contents.clear();
-        if ($gamePlayer && $gamePlayer._currentSpell) {
-            this.drawActorHp($gameParty.leader(), 0, 0, 200);
-            this.drawActorMp($gameParty.leader(), 0, 48, 200);
-            this.drawText("Spell: " + $dataSkills[$gamePlayer._currentSpell].name, 0, 96, 200);
+        
+        this.drawActorHp($gameParty.leader(), 0, 0, 200);
+        this.drawActorMp($gameParty.leader(), 0, 48, 200);
+        if ($gamePlayer && $gamePlayer._skillId) {
+            if ($dataSkills[$gamePlayer._skillId]) {
+                this.drawText("Spell: " + $dataSkills[$gamePlayer._skillId].name, 0, 96, 200);
+            }
         }
+        //this.drawText("Skill: ", 0, 96, 100)
     };
 
     HUD.prototype.windowWidth = function(){
